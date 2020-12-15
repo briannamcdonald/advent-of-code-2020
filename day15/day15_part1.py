a599eb877e5d9fcb0c311e2513d919a30c79a7f7
Brianna McDonald <brmcdonald@mun.ca>
2020-12-15 12:10:40 -0330

    day 15 completed

---
+++ day15/day15_part1.py
@@ -0,0 +1,23 @@
+def main():
+    nums = [0, 13, 16, 17, 1, 10, 6]
+    last_said = {}
+    counter = 1
+
+    for num in nums:
+        last_said[num] = counter
+        counter += 1
+
+    next_num = 0
+    while counter <= 2020:
+        current_num = next_num
+        if not current_num in last_said:
+            next_num = 0
+        else:
+            next_num = counter - last_said[current_num]
+        last_said[current_num] = counter
+        counter += 1
+    print(current_num)
+
+
+if __name__ == "__main__":
+    main()
